def571f4293eb8448d55515f3519ce5ca02ef618
Munorals <98166052+LeuanN@users.noreply.github.com>
2023-07-28 18:56:24 -0400

    Fix Kaw The Mammoth Destroyer
    Fix Kaw the mammoth destroyer

---
+++ emulador/SpellInfoCorrections.cpp
@@ -0,0 +1,10 @@
+// Fix Kaw The Mammoth Destroyer
+// Fixer: Leuan
+// Lines to Fix: 3584 to 3588
+// QuickFix: Just comment this code and the quest will be unbugged.
+
+    // Kaw the Mammoth Destroyer
+    //ApplySpellFix({ 46260 }, [](SpellInfo* spellInfo)
+    //{
+    //    spellInfo->AttributesEx2 |= SPELL_ATTR2_IGNORE_LINE_OF_SIGHT;
+    //});
