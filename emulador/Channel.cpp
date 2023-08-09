7b05928ef9bbb0d69d7cfbe06b3159bba600243f
Munorals <98166052+LeuanN@users.noreply.github.com>
2023-08-09 01:25:19 -0400

    Fix Spam LFG
    Se removió el "Se ha unido" y "Ha abandonado" de los canales.

---
+++ emulador/Channel.cpp
@@ -0,0 +1,7 @@
+// Editor: Leuan
+// File Name: Channel.cpp
+// Lines to Modify: 223 & 288
+// Modification: Remove Join/Leave Message from Chat
+
+223: //JoinNotify(Player);
+288: //LeaveNotify(Player);
